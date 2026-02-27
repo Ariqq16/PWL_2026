62819d116427491851371a72da743accaf8c6e14
Ariqq16 <ariqbaihaqi011@gmail.com>
2026-02-27 11:27:47 +0700

    js 2 route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/user/{name?}', function ($name=null) {
+return 'Nama saya '.$name;
 });
+
+
